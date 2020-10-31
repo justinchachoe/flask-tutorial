c20c38ba44e671a76d7919768dceea988568a482
justinchachoe <73210465+justinchachoe@users.noreply.github.com>
2020-10-31 17:46:50 +0700

    db.py, __init__.py, Initialized the database

--- flaskr/db.py
+++ flaskr/db.py
@@ -4,6 +4,10 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)    
+
 def init_db():
     db = get_db()
 
@@ -27,7 +31,7 @@ def get_db():
 
     return g.db
 
-def close_db():
+def close_db(e=None):
     db = g.pop('db', None)
 
     if db is not None:
